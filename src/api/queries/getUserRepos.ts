6f3dca05ac3f5a9b3b430e296a3dc18aba0d90f1
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-06-09 04:52:52 +0300

    add getUserRepos query

---
+++ src/api/queries/getUserRepos.ts
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+
+export const GET_USER_REPOS = gql`
+query getUserRepos($login: String!) {
+    repositoryOwner(login: $login) {
+        repositories(first: 5, orderBy: {field: UPDATED_AT, direction: DESC}) {
+            edges {
+                node {
+                    name
+                    owner {
+                        avatarUrl
+                        login
+                        url
+                    }
+                    stargazers {
+                        totalCount
+                    }
+                    homepageUrl
+                    description
+                }    
+            }
+        }
+    }
+}
+`;
