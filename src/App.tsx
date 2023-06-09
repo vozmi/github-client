37944d7fa7c9dc3bdca2e5838167db82d9c8ecaf
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-06-09 04:53:33 +0300

    call getUserQueries in App.tsx (test)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useEffect } from 'react'
-import './App.css'
+import { useQuery } from '@apollo/client';
+import './App.css';
+import { GET_USER_REPOS } from './api/queries/getUserRepos';
+import { useEffect } from 'react';
+import { GetUserReposQuery } from './gql/operation_types';
 
 function App() {
+  const { data } = useQuery<GetUserReposQuery>(GET_USER_REPOS, {
+    variables: {
+      login: "vozmi"
+    }
+  });
+
   useEffect(() => {
-    console.log("mount")
-  }, [])
+    console.debug(data);
+  }, [data]);
 
   return (
     <div>test</div>
